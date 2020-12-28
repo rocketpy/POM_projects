9e268b5a21355733d404d31616711a29b1201c3a
rocketpy <dimitr.ushakov@gmail.com>
2020-12-28 22:26:55 +0200

    Update result_page.py

--- project_1/pages/result_page.py
+++ project_1/pages/result_page.py
@@ -1,1 +1,30 @@
+from selenium.webdriver.common.by import By
 
+
+class DResultPage:
+    LINK_DIVS = (By.CSS_SELECTOR, '#')
+    SEARCH_INPUT = (By.ID, '')
+    
+    @classmethod
+    def words_results(cls, words):
+        xpath = f"//div[@id='links']//*[contains(text(), '{words}')]"
+        return (By.XPATH, xpath)
+      
+      
+    def __init__(self, browser):
+        self.browser = browser
+        
+        
+    def link_div_count(self):
+        link_divs = self.browser.find_elements(*self.LINK_DIVS)
+        return len(link_divs)
+      
+      
+    def phrase_result_count(self, phrase):
+        words_results = self.browser.find_elements(*self.PHRASE_RESULTS(phrase))
+        return len(words_results)
+      
+      
+    def search_input_value(self):
+        search_input = self.browser.find_element(*self.SEARCH_INPUT)
+        return search_input.get_attribute('value')
