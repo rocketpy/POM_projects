599d224e7c3f37d42e6d1460d15898141cf0b0a2
rocketpy <dimitr.ushakov@gmail.com>
2020-12-29 19:49:04 +0200

    added a main code

---
+++ project_1/tests.py
@@ -0,0 +1,28 @@
+import pytest
+from pages.result_page import ResultPage
+from pages.base_page import SearchPage
+from selenium.webdriver import Chrome
+
+
+#  to run test: pipenv run python -m pytest
+#  without venv: python -m pytest
+
+@pytest.fixture
+def browser():
+    driver = Chrome()
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
+    
+    
+def test_basic_duckduckgo_search(browser):
+    WORDS = 'cars'  # words for input 
+    search_page = SearchPage(browser)
+    search_page.load()
+    search_page.search(WORDS)
+    result_page = ResultPage(browser)
+    
+    assert result_page.link_div_count() > 0
+    assert result_page.phrase_result_count(WORDS) > 0
+    assert result_page.search_input_value() == WORDS
+    
