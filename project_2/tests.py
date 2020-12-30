414f5bee1788309d5b28c568aafdd82c689437dc
rocketpy <dimitr.ushakov@gmail.com>
2020-12-30 22:58:23 +0200

    Update tests.py

---
+++ project_2/tests.py
@@ -0,0 +1,12 @@
+from MainPage import Searcher
+
+
+def test_search(browser):
+    main_page = Searcher(browser)
+    main_page.go_to_site()
+    # main_page.enter_word("...")
+    # main_page.click_on_the_search_button()
+    elems = main_page.check_navigation_bar()
+    
+    assert "Images" and "Videos" in elems
+    
