8f69b89bf15c8acbe3bd92514500fc8c1283bf12
rocketpy <dimitr.ushakov@gmail.com>
2020-12-31 17:29:55 +0200

    Update base_page.py

--- project_2/base_page.py
+++ project_2/base_page.py
@@ -1,1 +1,9 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+
+class BasePage:
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.base_url = "https://www..."
