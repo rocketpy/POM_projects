464ea7d2e73d77bd15b89839abfc165e54204abf
rocketpy <dimitr.ushakov@gmail.com>
2020-12-31 17:34:27 +0200

    Update base_page.py

--- project_2/base_page.py
+++ project_2/base_page.py
@@ -7,3 +7,17 @@ class BasePage:
     def __init__(self, driver):
         self.driver = driver
         self.base_url = "https://www..."
+        
+        
+    def find_element(self, locator, time=10):
+        return WebDriverWait(self.driver, time).until(EC.presence_of_element_located(locator),
+                                                      message=f"Can't find element by locator {locator}")
+
+    def find_elements(self, locator, time=10):
+        return WebDriverWait(self.driver, time).until(EC.presence_of_all_elements_located(locator),
+                                                      message=f"Can't find elements by locator {locator}")
+
+    def go_to_website(self):
+        return self.driver.get(self.base_url)        
+
+    
