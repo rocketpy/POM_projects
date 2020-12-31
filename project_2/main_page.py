04daa3ef9a80bf9702a74e195ff117dd7b807f1d
rocketpy <dimitr.ushakov@gmail.com>
2020-12-31 17:21:50 +0200

    Update main_page.py

--- project_2/main_page.py
+++ project_2/main_page.py
@@ -1,1 +1,9 @@
+from base_page import BasePage
+from selenium.webdriver.common.by import By
+
+
+class SeacrhLocators:
+    LOCATOR_YANDEX_SEARCH_FIELD = (By.ID, "")
+    LOCATOR_YANDEX_SEARCH_BUTTON = (By.CLASS_NAME, "")
+    LOCATOR_YANDEX_NAVIGATION_BAR = (By.CSS_SELECTOR, "")
 
