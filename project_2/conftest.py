7f707b2468d45285e2226fafa36d8e406ba3f1e8
rocketpy <dimitr.ushakov@gmail.com>
2020-12-31 17:28:18 +0200

    Update conftest.py

--- project_2/conftest.py
+++ project_2/conftest.py
@@ -1,1 +1,9 @@
+import pytest
+from selenium import webdriver
 
+
+@pytest.fixture(scope="session")  #  one time per session
+def browser():
+    driver = webdriver.Chrome(executable_path="./chromedriver")
+    yield driver
+    driver.quit()
