f4d37d1f0d6b80b3cb6d670124f75b99c4d876aa
rocketpy <dimitr.ushakov@gmail.com>
2021-01-01 19:48:38 +0200

    Update tests.py

--- project_2/tests.py
+++ project_2/tests.py
@@ -10,3 +10,14 @@ def test_search(browser):
     
     assert "Images" and "Videos" in elems
     
+    
+def test_login(browser):
+    login_page = Loginer(browser)
+    login_page.go_to_website()
+    login_page.enter_word("Cars")
+    login_page.click_on_the_search_button()
+    
+    
+    assert "Images" and "Videos" in elems
+    
+    
