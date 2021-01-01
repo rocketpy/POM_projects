3e11e53a90e4fd13854fdcdb28cee2aace1e260a
rocketpy <dimitr.ushakov@gmail.com>
2021-01-01 20:01:15 +0200

    Update login_page.py

--- project_2/login_page.py
+++ project_2/login_page.py
@@ -1,1 +1,28 @@
+from base_page import BasePage
+from selenium.webdriver.common.by import By
 
+
+class LoginFormLocators:
+    LOCATOR_EMAIL_FIELD = (By.ID, "")
+    LOCATOR_PASSWORD_FIELD = (By.ID, "")
+    LOCATOR_LOGIN_BUTTON = (By.CLASS_NAME, "")
+
+
+class Loginer(BasePage):
+
+    def input_email(self, email):
+        email_field = self.find_element(LOginFormLocators.LOCATOR_EMAIL_FIELD)
+        email_field.send_keys(email)
+        return email_field
+
+      
+    def input_password(self, password):
+        password_field = self.find_element(LoginLocators.LOCATOR_PASSWORD_FIELD)
+        password_field.send_keys(password)
+        return password_field      
+    
+    
+    def click_on_login_button(self):
+        return self.find_element(LoginFormLocators.LOCATOR_LOGIN_BUTTON, time=2).click()
+
+ 
