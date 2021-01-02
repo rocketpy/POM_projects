de8e7b857360f4dba59d7dbc189fa31f44d511f0
rocketpy <dimitr.ushakov@gmail.com>
2021-01-02 22:01:48 +0200

    Update conftest.py

--- project_2/conftest.py
+++ project_2/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope="session")  #  one time per session
@@ -7,3 +8,26 @@ def browser():
     driver = webdriver.Chrome(executable_path="./chromedriver")
     yield driver
     driver.quit()
+
+    
+"""
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options )
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    print("\nquit browser..")
+    browser.quit() 
+"""
+    
