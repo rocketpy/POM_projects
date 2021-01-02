7a70407535a73e4174977843a2ccac5e2ec6db0f
rocketpy <dimitr.ushakov@gmail.com>
2021-01-02 22:02:21 +0200

    Update conftest.py

--- project_2/conftest.py
+++ project_2/conftest.py
@@ -29,5 +29,13 @@ def browser(request):
     yield browser
     print("\nquit browser..")
     browser.quit() 
+    
+    
+@pytest.mark.parametrize('language', ["ru", "en-gb"])
+def test_guest_should_see_login_link(browser, language):
+    link = f"http://selenium1py.pythonanywhere.com/{language}/"
+    browser.get(link)
+    browser.find_element_by_css_selector("#login_link")    
+    
 """
     
