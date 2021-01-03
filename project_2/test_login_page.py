1c863ccbaa428ecf2e3472473886e242ab5edfbb
rocketpy <dimitr.ushakov@gmail.com>
2021-01-03 16:59:01 +0200

    Update test_login_page.py

--- project_2/test_login_page.py
+++ project_2/test_login_page.py
@@ -1,1 +1,12 @@
+from login_page import Loginer
 
+
+def test_login(browser):
+    login_page = Loginer(browser)
+    login_page.go_to_website()
+    login_page.input_email("email@gmail")
+    login_page.input_password("password")
+    login_page.click_on_login_button()
+    
+    
+    assert #  need add assert check status !
