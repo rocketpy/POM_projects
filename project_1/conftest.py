ccc2f95c67f210b0215b54c289bc40f2b3565adf
rocketpy <dimitr.ushakov@gmail.com>
2021-01-05 23:11:49 +0200

    Update conftest.py

--- project_1/conftest.py
+++ project_1/conftest.py
@@ -1,1 +1,12 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
+
+#  pytest -s -v test_file_name.py
+
+@pytest.fixture(scope="function")  #  in case,  scope="session" , this fixture will run once by test
+def browser():
+    browser = webdriver.Chrome()
+    yield browser
+    browser.quit()
